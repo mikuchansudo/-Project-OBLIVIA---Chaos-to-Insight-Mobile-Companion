e46633db6e3840f65a85179ab3ed5407086be09d
mikuchansudo <mikuchan111000@gmail.com>
2025-04-17 00:44:46 +0530

    Create ClipboardListener.kt

---
+++ app/src/main/java/com/oblivia/ClipboardListener.kt
@@ -0,0 +1,12 @@
+```kotlin
+package com.oblivia
+
+import android.content.ClipboardManager
+import android.content.Context
+
+object ClipboardListener {
+    fun getClipboardText(context: Context): String {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        return clipboard.primaryClip?.getItemAt(0)?.text?.toString() ?: ""
+    }
+}
