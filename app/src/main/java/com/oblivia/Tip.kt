2db5cd030c86aaae1c4f142d66786eff1faa9d8e
mikuchansudo <mikuchan111000@gmail.com>
2025-04-17 00:45:42 +0530

    Create Tip.kt

---
+++ app/src/main/java/com/oblivia/Tip.kt
@@ -0,0 +1,12 @@
+```kotlin
+package com.oblivia
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tips")
+data class Tip(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val content: String
+)
+```
