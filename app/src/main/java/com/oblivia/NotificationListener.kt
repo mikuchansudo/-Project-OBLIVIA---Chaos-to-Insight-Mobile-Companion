2d413193ca0aad09425f7e1aad38440b4062f8a5
mikuchansudo <mikuchan111000@gmail.com>
2025-04-17 00:45:23 +0530

    Create NotificationListener.kt

---
+++ app/src/main/java/com/oblivia/NotificationListener.kt
@@ -0,0 +1,14 @@
+```kotlin
+package com.oblivia
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+
+class NotificationListener : NotificationListenerService() {
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val extras = sbn.notification.extras
+        val text = extras.getString("android.text") ?: return
+        // Handle storing/analyzing this notification if needed
+    }
+}
+```
