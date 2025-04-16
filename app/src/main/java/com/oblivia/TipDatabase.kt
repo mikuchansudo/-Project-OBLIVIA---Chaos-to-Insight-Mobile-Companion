1c2be1e48c41229ded334395019131ac13149308
mikuchansudo <mikuchan111000@gmail.com>
2025-04-17 00:46:30 +0530

    Create TipDatabase.kt

---
+++ app/src/main/java/com/oblivia/TipDatabase.kt
@@ -0,0 +1,29 @@
+```kotlin
+package com.oblivia
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Tip::class], version = 1)
+abstract class TipDatabase : RoomDatabase() {
+    abstract fun tipDao(): TipDao
+
+    companion object {
+        private var INSTANCE: TipDatabase? = null
+
+        fun getDatabase(context: Context): TipDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TipDatabase::class.java,
+                    "tip_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+```
